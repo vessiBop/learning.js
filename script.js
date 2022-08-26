cee7b99f09464d0609599c194d349c292641c533
Silvestre Fernandez <rasa356@outlook.com>
2022-08-26 16:43:38 +1200

    more javascript added

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Hello World from the external script.js file");
+// console.log("I enjoy writing code");
+/* console.log("This is written in a");
+console.log("Multi-line");
+console.log("otherwise known as block-comment");
+console.log("these commands will NOT be executed"); */
+console.log ("I hope you like my super complex website ;)");
+
+const myFirstName = "Silvestre";
+const myLastName = "Fernandez";
+
+/* Rule of thumb - Start with const, use as much as possible as it assigns a constant value to the variable
+e.g. const x = 2 if you need to change this for any reason, then change 'const' to 'let' but 
+avoid using var at all costs */
+
+console.log("The value in the variable myFirstName is ");
+console.log(myFirstName);
+console.log("The value for the variable myLastName is"); console.log(myLastName);
+
